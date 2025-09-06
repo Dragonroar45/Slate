abd89b6171ea011e7bf0512f52f5a052c53850b1
Dragonroar45 <malhotraharsh098@gmail.com>
2025-09-06 22:25:54 +0530

    Add favicon imgs and make pwa button

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa'; // You've installed this!
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Slate',
+        short_name: 'Slate',
+        description: 'A live, fast timetable for your campus schedule.',
+        theme_color: '#020617', 
+        background_color: '#020617', 
+        icons: [
+          {
+            src: 'pwa-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 });
